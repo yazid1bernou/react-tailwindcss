d7f7749a7a4505011256ed60e291644f68b181ee
unknown <yazid1bernou@gmail.com>
2025-07-02 16:18:52 +0100

    create header

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
+export default function App() {
+  return (
+    <>
+
+      <Header />
+      <Home />
+      <Footer />
+
+    </>
+  )
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+
+const Footer = () => {
+    return <footer>
+        <h1> This is Footer</h1>
+    </footer>
+}
+
+
+export default Footer;
